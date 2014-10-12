d442ec7dd046aa2c0840d4e29a965093712989e8
Brendan Murty <brendan@brendanmurty.com>
2014-10-12 18:38:35 +1100

    Styling tweak for navigation icons.

--- common/layout-header.php
+++ common/layout-header.php
@@ -4,7 +4,7 @@ $root_path = dirname(dirname(__FILE__));
 include_once $root_path.'/common/initialise.php';
 
 // Import update dates (added as a query string on the inserts)
-$date_update_css = "20141012b";
+$date_update_css = "20141012c";
 $date_update_js  = "20130903a";
 
 // Extract the current page's name
