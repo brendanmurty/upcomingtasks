6106959fd17b269f0541f7212562d68768d63253
Brendan Murty <brendan@brendanmurty.com>
2014-10-12 19:59:33 +1100

    Remove emphasis.

--- common/initialise.php
+++ common/initialise.php
@@ -13,7 +13,7 @@ include_once 'libs/database.php';
 // Setup common variables
 $app_name='UpcomingTasks';
 $app_info='UpcomingTasks is the simplified way to manage your Basecamp tasks when you\'re away from your computer.';
-$app_welcome='Quickly manage your projects and tasks, view progress and select a theme that suits your mood. <em>Free for all</em> Basecamp and Basecamp Personal accounts.';
+$app_welcome='Quickly manage your projects and tasks, view progress and select a theme that suits your mood. Free for all Basecamp and Basecamp Personal accounts.';
 $link_share='';
 $link_donate='';
 
