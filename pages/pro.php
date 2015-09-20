fe0fe10dc14230bae38937ed87f626ad9cff2f96
Brendan Murty <brendan@murty.id.au>
2015-09-20 11:58:21 +1000

    Fix grammar

--- pages/pro.php
+++ pages/pro.php
@@ -4,7 +4,7 @@ include_once $root_path . '/common/layout-header.php';
 ?>
 
 <h2><?= $app_name ?> Pro</h2>
-<p>For a small monthly cost, a <?= $app_name ?> Pro account gives access to additional features, including:</p>
+<p>For a small monthly cost, an <?= $app_name ?> Pro account gives access to additional features, including:</p>
 
 <ul class="features">
     <li class="released">Editing project details</li>
@@ -25,7 +25,7 @@ include_once $root_path . '/common/layout-header.php';
 <p>All current <?= $app_name ?> features will continue to work as they are for the life of the product. Your data will never be shared with any third-party and there will never be advertisements to get in the way.</p>
 
 <h3>Why should I subscribe?</h3>
-<p><?= $app_name ?> was created as a side-project in September 2012 by <a href="http://brendan.murty.id.au/">Brendan Murty</a>. Along with access to additional features, paying for a <?= $app_name ?> Pro account helps support future development and pay for ongoing maintenance costs.</p>
+<p><?= $app_name ?> was created as a side-project in September 2012 by <a href="http://brendan.murty.id.au/">Brendan Murty</a>. Along with access to additional features, paying for an <?= $app_name ?> Pro account helps support future development and pay for ongoing maintenance costs.</p>
 
 <h3>What if I want to cancel or pause my subscription?</h3>
 <p>You're free to downgrade to a free account at any time. There is no cancellation fee. You can re-subscribe again whenever you like.</p>
