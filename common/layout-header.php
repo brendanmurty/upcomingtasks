f9b057fb73a55004aba208d47a44c6e844525397
Brendan Murty <brendan@murty.id.au>
2015-09-20 12:03:05 +1000

    Improve Pro features list styling

--- common/layout-header.php
+++ common/layout-header.php
@@ -4,7 +4,7 @@ $root_path = dirname(dirname(__FILE__));
 include_once $root_path.'/common/initialise.php';
 
 // Asset update dates
-$date_update_css = "20150920b";
+$date_update_css = "20150920c";
 $date_update_js  = "20130903";
 
 // Extract the current page's name
