2ade127184dc5ab3aece6784b87cff129e1c6912
Brendan Murty <brendan@murty.id.au>
2015-10-12 19:38:27 +1100

    Improve 404 logic to attempt to find a relevant page

--- pages/404.php
+++ pages/404.php
@@ -3,42 +3,32 @@
 $root_path = dirname(dirname(__FILE__));
 include_once $root_path . '/libs/initialise.php';
 
-// Get the requested url, correct the format, split into sections
-$req=ltrim($_SERVER["REQUEST_URI"],'/');
-$req_parts=explode('?',$req);
-$req=str_ireplace('//','/',$req_parts['0']);
-$req_part=explode('/',$req_parts['0']);
-
-// Extract the query string variables
-$get_var='';
-if(isset($req_parts['1'])){
-	$get_variables=$req_parts['1'];
-	$get_var_item=explode('&',$get_variables);
-	foreach($get_var_item as $value){
-		$parts=explode('=',$value);
-		$get_var[$parts['0']]=$parts['1'];
-	}
+// Format the requested URL and extract page (e.g. /project/123) and query string (e.g. ?something=1&sample=yes)
+$path = explode('?', ltrim($_SERVER["REQUEST_URI"], '/'));
+$section = explode('/', $path['0']);
+$query = false;
+if (array_key_exists('1', $path)) {
+	$query = explode('&', $path['1']);
 }
 
-// Handle custom redirects or show 404
-if ($req) {
-	if (file_exists($root_path . '/pages/' . $req_part['0'] . '.php')) {
-	    // Attempt to find the requested page
-		if (user_exists() || $req_part['0'] == 'login' || $req_part['0'] == 'authenticate') {
-			// Set this as the current page
-			redirect('/pages/' . $req_part['0'] . '.php');
-		} elseif (!user_exists() && $req_part['1'] != 'account.php') {
-			// Redirect to login page
-			redirect('/pages/login.php');
+if (file_exists($root_path . '/pages/' . $section['0'] . '.php')) {
+    // A related page was found, redirect to it
+	if ($section['0'] == 'project' && array_key_exists('1', $section) && is_numeric($section['1'])) {
+		if (array_key_exists('3', $section) && is_numeric($section['3'])) {
+			// Redirect to selected task (e.g. /project/123/task/123)
+			redirect('/pages/task.php?project=' . $section['1'] . '&task=' . $section['3']);
+		} else {
+			// Redirect to selected project (e.g./project/123)
+			redirect('/pages/project.php?id=' . $section['1']);
 		}
-	}else{
-	    // 404 - page not found
-		if(!isset($page)){$page='';}
-		error_handle($page,'404 - Page not found','','');
-		redirect('/pages/home.php');
+	} else {
+		// Redirect to selected page (e.g. /more)
+		redirect('/pages/' . $section['0'] . '.php');
 	}
 } else {
-	// Redirect to the homepage
-	redirect('/pages/home.php');
+    // 404 - page not found
+	error_handle($_SERVER["REQUEST_URI"], '404 - Page not found', '', '');
 }
-?>
+
+// Default to redirecting to the home page
+redirect('/pages/home.php');
